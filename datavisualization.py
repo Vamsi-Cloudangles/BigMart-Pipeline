2dddb93809b3337f5a485122dc0f52113c25d53e
Vamsi-Cloudangles <111474104+Vamsi-Cloudangles@users.noreply.github.com>
2023-04-21 16:56:52 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -1,4 +1,5 @@
 import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
@@ -12,32 +13,31 @@ def data_visualization():
             categorical_columns.append(col)
         else:
             numerical_columns.append(col)
+
     # Visualize the categorical columns by using the count plot
     for i in categorical_columns:
-        fig,ax = plt.subplots(1,1, figsize=(5,4))
+        plt.subplots(1,1, figsize=(5,4))
         sns.countplot(x=dataset[i][1:])
-        fig.show()
     # Visualize the categorical columns by using the pie chart
-#     for c in categorical_columns:
-#         fig,plt.pie(dataset[c].value_counts(), labels = dataset[c].unique(), autopct='%1.2f%%')
-#         plt.show()
+    # for c in categorical_columns:
+    #   plt.pie(dataset[c].value_counts(), labels = dataset[c].unique(), autopct='%1.2f%%')
+        # plt.show()
     # Visualize the numerical columns using the distplot
     for i in numerical_columns:
-        fig, ax = plt.subplots(1,1, figsize=(8,4))
+        plt.subplots(1,1, figsize=(8,4))
         sns.distplot(x = dataset[i][1:])
     # Finding the outliers using the boxplot
-#     for n in numerical_columns:
-#         plt.boxplot(dataset[n])
-#         plt.xlabel(n)
-#         plt.show()
-    # Visualize the Correlation Matrix
+    # for n in numerical_columns:
+    #     plt.boxplot(dataset[n])
+    #     plt.xlabel(n)
+        # plt.show()
+
     corr = dataset.corr()
     fig, ax = plt.subplots(figsize=(10, 8))
     sns.heatmap(corr, cmap='RdBu', annot = True, fmt=".2f")
     plt.xticks(range(len(corr.columns)), corr.columns);
     plt.yticks(range(len(corr.columns)), corr.columns)
     plt.show()
-
     Q1 = dataset['Item_Visibility'].quantile(0.25)
     Q3 = dataset['Item_Visibility'].quantile(0.75)
     IQR = Q3 - Q1
